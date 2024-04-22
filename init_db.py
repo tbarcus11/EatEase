fbf16dd94bcb4ad6fdf646432d04bc42f009a6dd
tbarcus11 <tannerbarcus11@gmail.com>
2024-04-22 14:19:58 -0500

    html changes

--- init_db.py
+++ init_db.py
@@ -68,32 +68,45 @@ c.execute('''
     FOREIGN KEY (menu_item_id) REFERENCES menu_items(menu_item_id)
 );
 ''')
+#Starting user count at 10000
 
 c.execute("INSERT INTO users (id, username, password, email) VALUES (9999, 'dummy', 'dummy', 'dummy@example.com');")
 c.execute("DELETE FROM users WHERE id = 9999;")
 
-c.execute("INSERT INTO users (username, password, email, role) VALUES ('r', 'r', 'reg@gmail.com', 1)")
-c.execute("INSERT INTO users (username, password, email, role) VALUES ('o', 'o', 'mainowner@gmail.com', 2)")
+#Inserting base users
+
+c.execute("INSERT INTO users (username, password, email, role) VALUES ('customer', 'customer', 'reg@gmail.com', 1)")
+c.execute("INSERT INTO users (username, password, email, role) VALUES ('owner', 'owner', 'mainowner@gmail.com', 2)")
 c.execute("INSERT INTO users (username, password, email, role) VALUES ('mcds-owner', 'mcds-owner', 'mcdsowner@gmail.com', 2)")
 
-# Insert mock data
+# Inserting restraunts
+
 c.execute("INSERT INTO restaurants (name, location, owner) VALUES ('The Great Pizza', '123 Main St', 10001)")
 c.execute("INSERT INTO restaurants (name, location, owner) VALUES ('Burger House', '456 Side Ave', 10001)")
 c.execute("INSERT INTO restaurants (name, location, owner) VALUES ('Tans Tacos', '246 KU Ave', 10001)")
 c.execute("INSERT INTO restaurants (name, location, owner) VALUES ('McDonalds', '789 Des St', 10002)")
 
+# Inserting menu items
 
 c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (1, 'Margherita Pizza', 'Classic Margherita with fresh mozzarella and basil.', 12.99)")
 c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (1, 'Pepperoni Pizza', 'Pepperoni with a blend of cheeses.', 14.99)")
+c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (1, 'Sausage Pizza', 'Pizza with sausage topping', 12.99)")
 
 
 c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (2, 'Classic Burger', 'Beef burger with lettuce, tomato, and onion.', 10.99)")
 c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (2, 'Cheese Burger', 'Beef burger with cheese, lettuce, and tomato.', 11.99)")
+c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (2, 'Buffalo Wings', 'Classing wings with Ranch as a side', 12.99)")
 
-c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (3, 'McDouble', 'McDub', 3.99)")
-c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (3, 'Fries', 'Fries', 4.99)")
+c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (3, 'Steak Tacos', 'Grilled steak tacos, comes with 4 tacos', 10.99)")
+c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (3, 'Chicken Tacos', 'Grilled chicken tacos, comes with 4 tacos', 11.99)")
+c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (3, 'Pork Tacos', 'Roasted pork tacos, comes with 4 tacos', 12.99)")
 
-c.execute("INSERT INTO reviews (user_id, rest_id, rating, description) VALUES (10001, 1, 1, 'not good')")
+
+c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (4, 'McDouble', 'McDub', 3.99)")
+c.execute("INSERT INTO menu_items (rest_id, name, description, price) VALUES (4, 'Fries', 'Fries', 4.99)")
+
+# Inserting starter review
+c.execute("INSERT INTO reviews (user_id, rest_id, rating, description) VALUES (10000, 1, 1, 'Pizza is just not good.')")
 # Commit changes and close the connection
 conn.commit()
 conn.close()
